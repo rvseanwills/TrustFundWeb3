4f786de6df545b4e881a785e392b3e9e3dfdb551
rvseanwills <rvb.seanwills@gmail.com>
2025-11-18 15:16:26 +0000

    First

---
+++ TrustFund.sol
@@ -0,0 +1,25 @@
+/*
+
+• it should be able to accept SepETH (the currency for the Sepolia testnet) from anyone;
+• it should allow SepETH to be retrieved only by the following addresses:
+– 0x3cC92b7496571fC479EB4714784a6839CD3e57f2
+– your own address from which the contract is deployed
+• not more than 1 SepETH can be retrieved at a time; and
+• after a retrieval, the funds are locked for 30 minutes for all addresses.
+
+/*
+Successful deployment of your contract on the Sepolia testnet, evidenced by the transaction hash
+searchable on Etherscan. (5 marks)
+2. The contract should be able to accept SepETH at its own address. (10 marks) - https://dev.to/emanuelferreira/how-to-create-a-smart-contract-to-receive-donations-using-solidity-4k8
+3. The contract should allow retrieval of SepETH. (5 marks)
+1
+4. Only the two accounts specified above can retrieve SepETH from the contract. (5 marks)
+5. Retrieval of SepETH should be able to be done once per 30 min with a maximum value of 1 SepETH.
+(10 marks)
+6. Your contract should be re-entrancy attack resistant. (10 marks)
+7. Code quality - it should be well commented, and without obvious security vulnerabilities. (5 marks)
+*/
+
+contract TrustFund {
+
+}
